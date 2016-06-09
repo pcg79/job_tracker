a35adb6e3c15ba48bcd3a0a366fab21577901ac4
Pat George <pat.george@gmail.com>
2016-06-09 13:17:22 -0400

    Added Description to Jobs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609161142) do
+ActiveRecord::Schema.define(version: 20160609170150) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160609161142) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "state"
+    t.text     "description"
   end
 
   add_index "jobs", ["state"], name: "index_jobs_on_state"
