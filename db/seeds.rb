6ea9424a8cc0c0ff42f00169ab7b168e43626346
Pat George <pat.george@gmail.com>
2016-06-14 11:15:56 -0400

    Added jobs and positions to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+jobs = {
+  'Twitter' => 'Manager - Engineering',
+  'Facebook' => 'Tech Lead',
+  'Amazon' => 'Manager - Engineering - Flex',
+  'CustomInk' => 'Manager - Engineering',
+  'Dropbox' => 'Manager - Engineering - SEA',
+  'PhishMe' => 'Engineer',
+  'StitchFix' => 'Principal Engineer/Manager',
+  'CareZone' => 'Principal Engineer/Manager'
+}
+
+jobs.each do |company, job|
+  c = Company.create! name: company
+  c.jobs.create title: job
+end
+
